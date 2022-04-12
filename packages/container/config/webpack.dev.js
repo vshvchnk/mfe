b46ff6eb0059d82df7452ed87e9678bea0a6993c
vshvchnk <vshvchnk@gmail.com>
2022-04-13 02:13:24 +0300

    add prod container config

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageDeps = require('../package.json').dependencies;
@@ -20,9 +19,6 @@ const devConfig = {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js'
       },
       shared: packageDeps
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
     })
   ]
 };
