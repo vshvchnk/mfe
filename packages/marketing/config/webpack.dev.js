a7566db97f8bfae9bf8670c3d740b1119045c390
vshvchnk <vshvchnk@gmail.com>
2022-04-13 01:59:21 +0300

    initial commit

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,32 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageDeps = require('../package.json').dependencies;
+
+
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      },
+      shared: packageDeps
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+};
+
+module.exports = merge(commonConfig, devConfig);
